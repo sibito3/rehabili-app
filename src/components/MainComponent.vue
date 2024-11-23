d5c365745d27b095ac6f9646bcdd61501cbd9af0
taikobennto <sibitodaze1@yahoo.co.jp>
2024-11-23 14:59:30 +0900

    バグ修正や見た目の修正

--- src/components/MainComponent.vue
+++ src/components/MainComponent.vue
@@ -1,20 +1,49 @@
 <template>
 <div>
-    <div>This is Main Component</div>
-    <p>ルーレットお試し</p>
-    <div>{{ targetPrefectureList[resultIndex].name }}</div>
-    <button class="btn btn-primary" @click="startRoulette" v-if="isRouletteRunning === false">すた～と</button>
-    <button class="btn btn-primary" @click="stopRoulette" v-else>すとっぷ</button>
-    <div>ルーレットの結果から除外する都道府県を選択</div>
-    <div v-for="p in prefectureList" :key="p.id">
-        <div class="form-check">
-            <div class="check-custom">
-                <input v-bind:value="p.isExclude" v-on:change="onChangeCheckBox(p)" class="form-check-input" type="checkbox"  id="flexCheckDefault">
-            </div>
-            <label class="form-check-label" for="flexCheckDefault">
-                {{ p.name }}
-            </label>
+    <div class="main-container container">
+        <div>This is Main Component</div>
+        <div class="row title-area mt-3">
+            <div><h1>都道府県ルーレット</h1></div>
         </div>
+        <div class="row area-margin"></div>
+        <div class="row result-area">
+            <div class="d-flex justify-content-center">
+                <div class="result border border-dark-subtle border-3 rounded d-flex justify-content-center align-items-center">
+                    <div>{{ targetPrefectureList.length > 0 ? targetPrefectureList[resultIndex].name : "抽選対象がありません" }}</div>
+                </div>
+            </div>
+        </div>
+        <div class="row button-area mt-3">
+            <div>
+                <button class="btn btn-primary" @click="startRoulette" v-if="isRouletteRunning === false">すた～と</button>
+                <button class="btn btn-primary" @click="stopRoulette" v-else>すとっぷ</button>
+            </div>
+        </div>
+        <div class="row area-margin"></div>
+        <div class="row exclude-area">
+            <div class="card px-0 h-100">
+                <div  class="card-header">
+                    ルーレットの結果から除外する都道府県を選択
+                </div>
+                <div class="card-body overflow-auto">
+                    <div class="item-center">
+                        <ul class="list-item-left">
+                            <li v-for="p in prefectureList" :key="p.id">
+                                <div class="form-check">
+                                    <span class="check-custom">
+                                        <input v-bind:value="p.isExclude" v-on:change="onChangeCheckBox(p)" class="form-check-inline" type="checkbox" :id="`flexCheckPrefecture${p.id}`">
+                                    </span>
+                                    <label class="form-check-label" :for="`flexCheckPrefecture${p.id}`">
+                                        {{ p.name }}
+                                    </label>
+                                </div>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row area-margin"></div>
     </div>
 </div>
 </template>
@@ -52,6 +81,11 @@ methods: {
     },
     // チェックボックスの切り替え時
     onChangeCheckBox(checkedPrefecture: Prefecture) {
+        // if(this.targetPrefectureList.length === 1 && checkedPrefecture.isExclude === false) {
+        //     // 対象の都道府県リストが1でさらに減らそうとしたとき
+        //     alert("抽選対象をこれ以上減らすことができません！！");
+        //     return;
+        // }
         this.$store.dispatch('toggleExcludeFlag', checkedPrefecture);
     }
 },
@@ -69,9 +103,45 @@ data() {
 </script>
 
 <style>
-.check-custom {
-    display: inline-block;
-    margin-right: 0.2em;
+.area-margin{
+    margin-bottom: 10rem;
 }
+/* .main-container {
+    background-color: rgb(13, 70, 70);
+}
+.title-area{
+    background-color: rgb(83, 74, 163);
+}
+.result-area{
+    background-color: rgb(69, 15, 77);
+} */
+.result{
+    height: 10rem;
+    width: 10rem;
+    background-color: rgb(107, 4, 18);
+}
+.button-area{
+    /* background-color: rgb(39, 66, 8); */
+}
+.exclude-area{
+    /* background-color: rgb(66, 24, 8); */
+    height: 30rem;
+}
+.check-custom{
+ margin-right: -0.5em;
+}
+.list-item-left{
+    display: inline-block;
+    text-align: left;
+    list-style: none;
+}
+.item-center{
+    text-align: center;
+}
+/* .item-left{
+    display: flex;
+    justify-content: left;
+    
+} */
 </style>
   
